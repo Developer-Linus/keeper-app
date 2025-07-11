7f6dd15ae03f68263eda96ced646095bc99d65ed
Developer-Linus <linusmksu30@gmail.com>
2025-07-11 18:42:45 +0300

    Setup for Keeper project.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header.jsx';
+import Note from './components/Note.jsx';
+import Footer from './components/Footer.jsx';
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Note/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
+
