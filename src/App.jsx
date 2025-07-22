b019c90642b276cf081dfed97861bc162568fce7
Developer-Linus <linusmksu30@gmail.com>
2025-07-22 10:16:38 +0300

    Changed Note component to accept props.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import Header from './components/Header.jsx';
 import Note from './components/Note.jsx';
 import Footer from './components/Footer.jsx';
+import notes from "./notes.js";
 
 const App = () => {
+    const createNote = (note) =>{
+        return (
+            <Note
+                key = {note.key}
+                title = {note.title}
+                content = {note.content}
+            />
+        )
+    }
   return (
     <>
       <Header/>
-      <Note/>
+        {notes.map(createNote)}
       <Footer/>
     </>
   )
